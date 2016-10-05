1ea203537e72db01c1421d56d7affc6ca49ab93c
StaphSynth <StaphSynth@users.noreply.github.com>
2016-10-06 00:27:42 +1100

    added more tests. They aren't very robust, but it's a start...

--- testCode.js
+++ testCode.js
@@ -11,7 +11,7 @@ TO USE:
   * Install node.js
   * Then use the NPM to install selenium-webdriver
 
-Make sure you have the Chrome browser driver installed on your machine
+Note to self: Make sure you have the Chrome browser driver installed on your machine
 and that it's included in your $PATH
 See: https://christopher.su/2015/selenium-chromedriver-ubuntu/
 
@@ -36,38 +36,107 @@ var driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 
+//mocha test environment stuff
+//var test = require('/usr/local/node_modules/selenium-webdriver/testing');
+//var assert = require('assert');
+
 //poll the DOM for change state for 5 sec before declaring failure.
 //Not ideal. I hate hard-coding magic numbers
 driver.manage().timeouts().implicitlyWait(5000);
+
+
 
 /******************************
 TESTING FUNCTIONS
 ******************************/
 
+/*** TEST ABILITY TO ADD A TO-DO ITEM ***/
+function addToDo(inputText){
+  //go to page
+  driver.get('http://todomvc.com/');
+  //click link
+  driver.findElement(By.linkText('AngularJS')).click();
+  //select text area element and input text
+  driver.findElement(By.id('new-todo')).sendKeys(inputText + '\n').then(function(){
+    //now verify that it's been added to the list by looping through the list items and checking against innerHTML
+    var toDoItems = driver.findElements(By.className('ng-binding')).then(function(toDoItems){
+      for(var i = 0; i < toDoItems.length; i++) {
+        toDoItems[i].getText().then(function(text){
+          if(text === inputText) {
+            console.log("Item '" + inputText + "' successfully added");
+          }
+        });
+      }
+    });
+  });
+}
 
+/*** TEST ABILITY TO MODIFY AN EXISTING TO-DO ITEM ***/
+function modifyItem(newValue) {
+  var originalValue = "Here's some default text for the list item";
+  //first, create an item to modify
+  addToDo(originalValue);
+    //then find and modify the item
+    var toDoItems = driver.findElements(By.className('ng-binding')).then(function(toDoItems){
+      for(var i = 0; i < toDoItems.length; i++) {
+        toDoItems[i].getText().then(function(itemText){
+          if(itemText === originalValue) {
+            //return true;
+          }
+        });
+      }
+    });
+}
+
+/*** TEST ABILITY TO COMPLETE TO-DO ITEM ***/
+/*This is obviously not a very robust test. It doesn't check the innerHTML
+of the list item to ensure it's actually the one we're looking for,
+and assumes there were no previously completed items.
+However, I've had all sorts of trouble traversing the DOM to the
+child elements to check that.
+
+So here it is, warts and all...*/
+function complete() {
+
+  var itemText = "Some list item text";
+  //create item to complete
+  addToDo(itemText);
+  //click the button to mark complete
+  driver.findElement(By.className('toggle')).click().then(function(){
+  //look for a class with 'completed' which didn't exist in the DOM before
+    if(driver.findElement(By.className('completed')))
+      console.log("Task marked as completed");
+  });
+}
+
+/*** TEST ABILITY TO UN-COMPLETE (RE-ACTIVATE) TO-DO ITEM ***/
+/*Again, this is not a robust test and makes the (silly) assumption that
+there was only one 'completed' task.
+
+I really wish I was better at traversing the DOM to verify this better,
+but I just don't have the time to figure out what's going wrong... I miss jQuery *sniff* ;)*/
+function unComplete() {
+  var itemText = "Some test data";
+  //create item and mark as complete
+  complete();
+
+  //now click the button again and see if the 'completed' class disappears
+  driver.findElement(By.className('toggle')).click();
+
+  if(!(driver.findElement(By.className('completed'))))
+    console.log("Task marked as uncomplete");
+}
 
 /******************************
 ACTUAL TESTS
 ******************************/
-//TEST ABILITY TO INPUT TEXT TO LIST
-var inputText = "BLAH BLAH BLAH!";
-//go to page
-driver.get('http://todomvc.com/');
-//click link
-driver.findElement(By.linkText('AngularJS')).click();
-//select text area element and input text
-driver.findElement(By.id('new-todo')).sendKeys(inputText + '\n').then(function(){
-  //now verify that it's been added to the list
-  var toDoItems = driver.findElements(By.className('ng-binding')).then(function(toDoItems){
-    for(var i = 0; i < toDoItems.length; i++) {
-      toDoItems[i].getText().then(function(text){
-        if(text === inputText) {
-          console.log("Item '" + inputText + "' successfully added");
-        }
-      });
-    }
-  });
-});
+
+//addToDo("some text here");
+//driver.quit();
+
+//modifyItem("new list item value");
+
+complete();
 
 //kill the browser when done
-driver.quit();
+//driver.quit();
