9db4c22f42bbab662dabfb1e9114edb456efa187
StaphSynth <StaphSynth@users.noreply.github.com>
2016-10-05 15:57:02 +1100

    sorted code into functional blocks

--- testCode.js
+++ testCode.js
@@ -7,7 +7,7 @@ TEST SCRIPT FOR http://todomvc.com/examples/angularjs/#/ BY DAVID ALLEN
 This test script is written in JavaScript and uses the Node.js selenium-webdriver
 node module to drive the browser.
 
-To USE (Debian-based Linux): Install node.js
+TO USE (Debian-based Linux): Install node.js
 $ sudo apt-get install node.js
 Then use the NPM to install selenium-webdriver
 $ sudo npm install selenium-webdriver
@@ -18,8 +18,12 @@ See: https://christopher.su/2015/selenium-chromedriver-ubuntu/
 
 ==========================================================================*/
 
+/******************************
+ENVIRONMENT SET-UP
+******************************/
+
 /*
-  This is brittle, it's pointing to  the location of the module on my machine.
+  This is brittle, it's pointing to the location of the module on my machine.
   You'll have to change that to
   /PATH/TO/YOUR_NODE_INSTALLATION/selenium-webdriver
   to make this work on your machine
@@ -33,10 +37,51 @@ var driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 
-//navigate to the start page
-driver.get('http://todomvc.com/');
-//click on the link to the page we want to test
-driver.findElement(By.linkText('AngularJS')).click();
+//poll the browser for change state for 5 sec before declaring failure.
+//Not ideal. I hate hard-coding magic numbers
+driver.manage().timeouts().implicitlyWait(5000);
 
+/******************************
+TESTING FUNCTIONS
+******************************/
+
+/*** loads the page we want to test, beginning at http://todomvc.com/ ***/
+function loadPage() {
+
+  //navigate to the start page
+  if(!(driver.get('http://todomvc.com/'))) {
+    console.log('ERROR: Failed to resolve todomvc.com');
+    return false
+  }
+
+  //click on the link to the page we want to test
+  if(!(driver.findElement(By.linkText('AngularJS')).click())) {
+    console.log('ERROR: click link "AngularJS" failed');
+    return false;
+  }
+
+  console.log('loadPage PASSED');
+  return true;
+}
+
+/*** adds a new item to the todo list ***/
+function addToDo(text) {
+  loadPage();
+
+  //select 'to do' field and send keystrokes
+  driver.findElement(By.id('new-todo')).sendKeys(text + '\n');
+
+  //make sure element is added to list
+  var items = driver.findElements(By.className('ng-binding'));
+  
+
+}
+
+/******************************
+ACTUAL TESTS
+******************************/
+
+
+addToDo("TESTING TEXT ITEM 1");
 
 // driver.quit();
