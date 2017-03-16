30c8d5a94103c6da1377fda93658ff9c9aa08090
StaphSynth <StaphSynth@users.noreply.github.com>
2017-03-16 23:04:09 +1100

    add protractor config

---
+++ protractor.conf.js
@@ -0,0 +1,13 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  specs: ['todo.spec.js'],
+
+  multiCapabilities: [{
+    'browserName': 'chrome'
+  },
+  {
+    'browserName': 'firefox'
+  }]
+
+};
